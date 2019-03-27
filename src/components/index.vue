a8fa09bbd799dfc631e6379e7346d0e905fba353
w-1311 <1620575193@qq.com>
2019-03-28 01:58:58 +0800

    首页热卖数据渲染

--- src/components/index.vue
+++ src/components/index.vue
@@ -150,50 +150,14 @@
           <!--/幻灯片-->
           <div class="left-220">
             <ul class="side-img-list">
-              <li>
+              <li v-for="(item,index) in toplist" :key="index">
                 <div class="img-box">
-                  <label>1</label>
-                  <img src="http://39.108.135.214:8899/imgs/SJ4EgwosX0wTqvyAvhtFGT1w.jpg">
+                  <label>{{ index+1 }}</label>
+                  <img :src="item.img_url">
                 </div>
                 <div class="txt-box">
-                  <a href="/goods/show-98.html">骆驼男装2017秋季新款运动休闲纯色夹克青年宽松长袖针织开衫卫衣</a>
-                  <span>2017-09-26</span>
-                </div>
-              </li>
-              <li>
-                <div class="img-box">
-                  <label>2</label>
-                  <img
-                    src="http://39.108.135.214:8899/upload/201504/20/thumb_201504200314272543.jpg"
-                  >
-                </div>
-                <div class="txt-box">
-                  <a href="/goods/show-98.html">奔腾（BNTN） 380功放+纽约至尊 套装家庭影院</a>
-                  <span>2015-04-20</span>
-                </div>
-              </li>
-              <li>
-                <div class="img-box">
-                  <label>3</label>
-                  <img
-                    src="http://39.108.135.214:8899/upload/201504/20/thumb_201504200318534459.jpg"
-                  >
-                </div>
-                <div class="txt-box">
-                  <a href="/goods/show-98.html">飞利浦（PHILIPS）DVP3690 全高清DVD影碟机播放器</a>
-                  <span>2015-04-20</span>
-                </div>
-              </li>
-              <li>
-                <div class="img-box">
-                  <label>4</label>
-                  <img
-                    src="http://39.108.135.214:8899/upload/201504/20/thumb_201504200258403759.jpg"
-                  >
-                </div>
-                <div class="txt-box">
-                  <a href="/goods/show-98.html">三星（SAMSUNG）UA40HU5920JXXZ 40英寸4K超高清</a>
-                  <span>2015-04-20</span>
+                  <a href="/goods/show-98.html">{{ item.title }}</a>
+                  <span>{{ item.add_time | formatTime }}</span>
                 </div>
               </li>
             </ul>
@@ -648,8 +612,35 @@
 </template>
 
 <script>
+// 导入axios 先cnpm i axios --save
+import axios from "axios";
+
 export default {
-    name:"首页",
+  name: "首页",
+  // 数据
+  data() {
+    return {
+      // 分类数据
+      catelist: [],
+      // 轮播图数据
+      sliderlist: [],
+      // 热卖数据
+      toplist: [],
+    };
+  },
+ 
+  // 钩子函数 Vue实例创建完毕，并且组件的属性都设置完毕之后触发
+  created() {
+    axios
+      .get("http://111.230.232.110:8899/site/goods/gettopdata/goods")
+      .then(res => {
+        // console.log(res);
+        // 赋值 给该数组中的data属性
+        this.catelist = res.data.message.catelist;
+        this.sliderlist = res.data.message.sliderlist;
+        this.toplist = res.data.message.toplist;
+      });
+  }
 };
 </script>
 
